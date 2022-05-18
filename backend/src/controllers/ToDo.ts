0dac0e78ea5ab5536d7485aec1c4c6fda839b7b0
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-18 17:20:46 -0300

    feat: implementa o metodo read no controller de ToDo

--- backend/src/controllers/ToDo.ts
+++ backend/src/controllers/ToDo.ts
@@ -3,6 +3,11 @@ import ToDoService from '../services/ToDo';
 import ControllerErrors from '../enums/ControllerErrors';
 import StatusCode from '../enums/StatusCode';
 import { ToDo } from '../interfaces/ToDoInterface';
+import RequestIncrement from '../interfaces/RequestIncrement';
+
+export type ResponseError = {
+  error: unknown,
+};
 
 class ToDoController {
   service: ToDoService;
@@ -20,5 +25,31 @@ class ToDoController {
 
   get route() { return this._route; }
 
-  
+  create = async (
+    req: RequestIncrement<ToDo>,
+    res: Response<ToDo | ResponseError>,
+  ): Promise<typeof res> => {
+    try {
+      const created = await this.service.create(req.body);
+      if (!created) {
+        return res.status(this.status.INTERNAL_SERVER_ERROR)
+          .json({ error: this.errors.internalServerError });
+      }
+      if ('error' in created) {
+        return res.status(this.status.B_R).json({ error: this.errors.BR });
+      }
+      return res.status(this.status.CREATED).json(created);
+    } catch (e) {
+      return res
+        .status(this.status.INTERNAL_SERVER_ERROR) 
+        .json({ error: this.errors.internalServerError });
+    }
+  };
+
+  read = async (_req: Request, res: Response) => {
+    const ToDos = await this.service.read();
+    return res.status(this.status.OK).json(ToDos);
+  };
 }
+
+export default ToDoController;
