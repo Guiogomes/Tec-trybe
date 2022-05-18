c631c5bc8cce6071726a33d966dd7ae0951f2058
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-18 16:48:01 -0300

    refactor: faz com que as chaves status e description sejam opcionais no ToDoSchema e da um valor default para a chave status

--- backend/src/interfaces/ToDoInterface.ts
+++ backend/src/interfaces/ToDoInterface.ts
@@ -1,12 +1,15 @@
 import { z } from 'zod';
 
 export const ToDoSchema = z.object({
-  status: z.enum(['Pendente', 'Em andamento', 'Concluído']),
+  status: z
+    .enum(['Pendente', 'Em andamento', 'Concluído'])
+    .optional()
+    .default('Pendente'),
   title: z.string({
     required_error: 'Título da tarefa é obrigatório',
     invalid_type_error: 'Título da tarefa deve ser uma string',
   }),
-  description: z.string(),
+  description: z.string().optional(),
   dueDate: z.date(),
 });
 
