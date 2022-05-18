544d5ebd696d4d2e2781cbba75eb691b30e8cebd
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-18 17:29:16 -0300

    feat: implementa a classe Route e começa a gerar as rotas para o controller de ToDo

---
+++ backend/src/routes/Routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import ToDoController from '../controllers/ToDo';
+
+class GenericRouter {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+  }
+
+  public addRoutes(
+    controller: ToDoController,
+    route: string = controller.route,
+  ): void {
+    this.router.get(`${route}`, controller.read);
+    // this.router.get(`${route}/:id`, controller.readOne);
+    this.router.post(`${route}`, controller.create);
+    // this.router.put(`${route}/:id`, controller.update);
+    // this.router.delete(`${route}/:id`, controller.delete);
+  }
+}
+
+export default GenericRouter;
