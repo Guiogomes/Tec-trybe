e404ba7ba3d9c627a881effedc48e4d2ea74d416
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-19 19:43:07 -0300

    feat: implementa as funções de organizar a tabela e os botões para chamarem elas

--- frontend/src/Components/HiddenForm.jsx
+++ frontend/src/Components/HiddenForm.jsx
@@ -1,12 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { MyContext } from '../context/Provider';
 import '../css/FillButton.css';
 
 const FillAgenda = () => {
-  const { setHidden } = useContext(MyContext);
+  const { setHidden, setToDos, toDos } = useContext(MyContext);
 
   const handleClick = () => {
     setHidden(false);
+  }
+
+  const handleSortTitleClick = () => {
+    const sortedTitleTodos = toDos.sort((a, b) => {
+      if (a.title < b.title) return -1;
+      if (a.title > b.title) return 1;
+      return 0;
+    });
+    setToDos(sortedTitleTodos);
+    console.log(toDos); 
+  }
+
+  const handleSortDateClick = () => {
+    const sortedDateTodos = toDos.sort((a, b) => a.dueDate.getTime() - b.dueDate.getTime());
+    setToDos(sortedDateTodos);
+  }
+
+  const handleSortStatusClick = () => {
+    const sortedStatusTodos = toDos.sort((a, b) => {
+      if (a.status === 'Pendente') {
+        return a.status > b.status;
+      } if (a.status === 'Em andamento') {
+        return a.status < b.status;
+      } if (a.status === 'Concluído') {
+        return 0;
+      }
+    });
+    setToDos(sortedStatusTodos);
+    console.log(toDos)
   }
 
   return (
@@ -16,6 +45,24 @@ const FillAgenda = () => {
         onClick={handleClick}
       >
         Preencher tarefa
+      </button>
+      <button
+        className='button'
+        onClick={handleSortTitleClick}
+      >
+        Ordenar por title
+      </button>
+      <button
+        className='button'
+        onClick={handleSortDateClick}
+      >
+        Ordenar por data
+      </button>
+      <button
+        className='button'
+        onClick={handleSortStatusClick}
+      >
+        Ordenar por status
       </button>
     </div>
   )
